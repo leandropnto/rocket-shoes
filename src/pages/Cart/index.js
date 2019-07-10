214722f8fd47e683103cff09ce84b955d2ccb7f2
lpinto <lpinto@crm-rj.gov.br>
2019-07-10 18:55:07 -0300

    Commit inicial

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
+
+export default function Cart() {
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_detalhe2.jpg?resize=326:*"
+                alt="tenis"
+              />
+            </td>
+            <td>
+              <strong>Tênis muito massa</strong>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline color="#7159c1" size={20} />
+                </button>
+                <input type="numer" value={1} readOnly />
+                <button type="button">
+                  <MdAddCircleOutline color="#7159c1" size={20} />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete color="#7159c1" size={20} />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$ 1920,28</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
+}
